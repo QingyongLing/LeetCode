5672df07f2e6e43709aeeab16bd9b27fc82860fe
Ling <2682451149@qq.com>
2016-05-16 12:23:06 +0800

    9+11

---
+++ 009-Palindrome Number/009.cpp
@@ -0,0 +1,43 @@
+//Determine whether an integer is a palindrome. Do this without extra space.
+//click to show spoilers.
+//Some hints:
+//Could negative integers be palindromes? (ie, -1)
+//If you are thinking of converting the integer to string, note the restriction of using extra space.
+//You could also try reversing an integer. However, if you have solved the problem "Reverse Integer", you know that the reversed integer might overflow. How would you handle such case?
+//There is a more generic way of solving this problem.
+//
+#include <assert.h>
+class Solution2 {
+public:
+  //Runtime: 76 ms
+	bool isPalindrome(int x) {
+		if (x < 0)
+			return false;
+		int num = 1;
+		while (x / num >= 10)
+			num *= 10;
+		while (x)
+		{
+			int left = x / num;
+			int right = x % 10;
+			if (left != right)
+				return false;
+			x -= num*left;
+			num /= 100;
+			x /= 10;
+		}
+		return true;
+	}
+};
+
+int main()
+{
+
+	Solution2 s;
+	assert(s.isPalindrome(121));
+	assert(s.isPalindrome(1));
+	assert(!s.isPalindrome(-1));
+	assert(!s.isPalindrome(-121));
+	assert(s.isPalindrome(11));
+	return 0;
+}
---
+++ 011-Container With Most Water/011.cpp
@@ -0,0 +1,55 @@
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+//Given n non - negative integers a1, a2, ..., an, where each represents a point at coordinate(i, ai).
+//n vertical lines are drawn such that the two endpoints of line i is at(i, ai) and(i, 0).Find two lines,
+//which together with x - axis forms a container, such that the container contains the most water.
+//Note: You may not slant the container.
+
+class Solution {
+public:
+	//Time Limit Exceeded
+	int maxArea(vector<int>& height) {
+		int maxcontainer = 0;
+		int last = 0;
+		int left = 0, right = height.size() - 1;
+		for (int i = 0; i < height.size(); ++i)
+		{
+			if (height[i]<last)
+			   left = 0,right=height.size()-1;
+			while (height[left] < height[i]){ ++left; }
+			while (height[right] < height[i]){ --right; }
+			maxcontainer = max(maxcontainer, max(i - left, right - i)*height[i]);
+			last = height[i];
+		}
+		return maxcontainer;
+	}
+	int maxArea2(vector<int> &height)
+	{
+		int left = 0, right = height.size() - 1, maxcontainer = 0;
+		while (left < right)
+		{
+			maxcontainer = max(maxcontainer, (right - left)*min(height[left], height[right]));
+			if (height[left]<height[right])
+				do
+				{
+				++left;
+				} while (left<right&&height[left]<=height[left - 1]);
+			else
+				do
+				{
+				--right;
+				} while (left<right&&height[right]<=height[right + 1]);
+		}
+		return maxcontainer;
+	}
+};
+int main()
+{
+  Solution s;
+  vector<int> temp={ 1, 2, 3, 1, 1 };
+	cout << s.maxArea2(temp);
+  return 0;
+}
