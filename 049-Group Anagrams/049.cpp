2721ac4f1f876b59245b4d61b6d7ec6b8a7bd1c1
Ling <1027837999@qq.com>
2016-07-22 22:32:35 +0800

    49

---
+++ 049-Group Anagrams/049.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <string>
+#include <map>
+#include <functional>
+using namespace std;
+/*
+Given an array of strings, group anagrams together.
+For example, given: ["eat", "tea", "tan", "ate", "nat", "bat"],
+Return:
+[
+["ate", "eat","tea"],
+["nat","tan"],
+["bat"]
+]
+Note: All inputs will be in lower-case.
+*/
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> result;
+        if (strs.size() == 1)
+        {
+            result.push_back(strs);
+            return result;
+        }
+        else
+        {
+            auto old = strs;
+            for (auto &s : strs)
+                sort(s.begin(), s.end());
+            map<size_t, vector<int>> m;
+            std::hash<std::string> hash_fn;
+            for (int i = 0; i < strs.size(); ++i)
+            {
+                m[hash_fn(strs[i])].push_back(i);
+            }
+            for (auto &s : m)
+            {
+                vector<string> temp;
+                for (auto &c : s.second)
+                    temp.push_back(move(old[c]));
+                result.push_back(temp);
+            }
+        }
+        return result;
+    }
+
+    vector<vector<string>> groupAnagrams2(vector<string>& strs) {
+        vector<vector<string>> result;
+        if (strs.size() == 1)
+        {
+            result.push_back(strs);
+            return result;
+        }
+        else
+        {
+            auto fun_hash = [](const string& str)->string
+            {
+                int num[26] = {};
+                for (auto &c : str)
+                    ++num[c - 'a'];
+                string temp;
+                for (int i = 0; i < 26; ++i)
+                {
+                    if (num[i] != 0)
+                    {
+                        temp.push_back(static_cast<char>(num[i]));
+                        temp.append(",");
+                    }
+                    else
+                        temp.append(",");
+                }
+                return temp;
+            };
+            map<size_t, vector<int>> m;
+            std::hash<std::string> hash_fn;
+            for (int i = 0; i < strs.size(); ++i)
+            {
+                m[hash_fn(fun_hash(strs[i]))].push_back(i);
+            }
+            for (auto &s : m)
+            {
+                vector<string> temp;
+                for (auto &c : s.second)
+                    temp.push_back(move(strs[c]));
+                result.push_back(temp);
+            }
+        }
+        return result;
+    }
+};
+int main()
+{
+    Solution s;
+    vector<string> vec = { "eat", "tea", "tan", "ate", "nat", "bat" };
+    auto result = s.groupAnagrams2(vec);
+    for (auto &s : result)
+    {
+        for (auto &c : s)
+            cout << c << " ";
+        cout << endl;
+    }
+    system("pause");
+    return 0;
+}
