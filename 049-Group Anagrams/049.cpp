bd94e7f25bb149fcd769c33c3dd8a970b6c11fe0
Ling <1027837999@qq.com>
2016-07-23 12:46:09 +0800

    49

--- 049-Group Anagrams/049.cpp
+++ 049-Group Anagrams/049.cpp
@@ -104,10 +104,8 @@ public:
             auto fun_hash = [](const string& str)->size_t
             {
                 size_t temp = 0;
-                for (auto &c : str)
-                {
-                    temp += c*c*c*c;
-                }
+                for (auto &c : str)               
+                    temp += c*c*c*c;               
                 return temp;
             };
             map<size_t, vector<int>> m;
@@ -125,12 +123,88 @@ public:
         }
         return result;
     }
+    // beats 99.92% of cppsubmissions  233333 
+    vector<vector<string>> groupAnagrams4(vector<string>& strs) {
+        vector<vector<string>> result;
+        if (strs.size() == 1)
+        {
+            result.push_back(strs);
+            return result;
+        }
+        else
+        {
+            auto fun_hash = [](const string& str)->size_t
+            {
+                size_t temp = 0;
+                for (auto &c : str)               
+                    temp += c*c*c*c;              
+                return temp;
+            };
+            vector<pair<size_t, string*>> data;
+            for (auto &s : strs)
+                data.push_back(make_pair(fun_hash(s), &s));
+            sort(data.begin(), data.end(), [](pair<size_t, string*>& left, pair<size_t, string*>& right)
+            {return left.first < right.first; });
+
+            size_t temp = data[0].first;
+            vector<string> tempstr;
+            for (auto &p : data)
+            {
+                if (p.first != temp)
+                {
+                    temp = p.first;
+                    result.push_back(tempstr);
+                    tempstr.clear();
+                    tempstr.push_back(*p.second);
+                }
+                else
+                    tempstr.push_back(*p.second);
+            }
+            result.push_back(tempstr);
+        }
+        return result;
+    }
+    vector<vector<string>> groupAnagrams5(vector<string>& strs) {
+        vector<vector<string>> result;
+        if (strs.size() == 1)
+        {
+            result.push_back(strs);
+            return result;
+        }
+        else
+        {
+            vector<string> sorted_strs = strs;
+            for (auto &str : sorted_strs)
+                sort(str.begin(), str.end());
+            vector<pair<string*, string*>> data;
+            for (int i = 0; i < strs.size(); ++i)
+                data.push_back(make_pair(&sorted_strs[i], &strs[i]));
+            sort(data.begin(), data.end(),[](pair<string*, string*> &left, pair<string*, string*> &right)->bool
+            {return *left.first < *right.first; });
+            vector<string> temp;
+            for (int i = 0; i < data.size(); ++i)
+            {
+                if (i != 0 && *data[i].first != *data[i - 1].first)
+                {
+                    result.push_back(temp);
+                    temp.clear();
+                    temp.push_back(*data[i].second);
+                }
+                else
+                {
+                    temp.push_back(*data[i].second);
+                }
+            }
+            result.push_back(temp);
+        }
+        return result;
+    }
 };
 int main()
 {
     Solution s;
     vector<string> vec = { "eat", "tea", "tan", "ate", "nat", "bat" };
-    auto result = s.groupAnagrams2(vec);
+    auto result = s.groupAnagrams5(vec);
     for (auto &s : result)
     {
         for (auto &c : s)
