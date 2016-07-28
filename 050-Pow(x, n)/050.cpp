e7bfc2930467e6356a8d1f840983e244adbe3c32
Ling <1027837999@qq.com>
2016-07-28 20:58:11 +0800

    50

---
+++ 050-Pow(x, n)/050.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+/*
+Implement pow(x, n).
+*/
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if (n == 0)
+            return 1.0;
+        if (n == 1)
+            return x;
+        double result = 1.0;
+        if (n == INT_MIN)
+        {
+            fun(x, result, INT_MAX);
+            result *= x;
+        }
+        else
+        {
+            int tempn = n > 0 ? n : -n;
+            fun(x, result, tempn);
+        }     
+        return n>0 ? result : 1.0/result;
+    }
+    void fun(double x, double& result, int n)
+    {
+        if (n == 0)
+            return;
+        if (n % 2 == 0)
+            fun(x*x, result, n / 2);
+        else
+        {
+            result *= x;
+            fun(x, result, n - 1);
+        }
+    }
+};
+int main()
+{
+    Solution s;
+    cout << s.myPow(1.0, INT_MIN) << endl;
+    system("pause");
+    return 0;
+}
