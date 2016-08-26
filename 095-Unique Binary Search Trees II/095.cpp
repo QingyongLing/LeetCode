81e403a97acd144cd240a625bb2045ea569f52aa
Ling <1027837999@qq.com>
2016-08-26 16:27:50 +0800

    95

---
+++ 095-Unique Binary Search Trees II/095.cpp
@@ -0,0 +1,86 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+/*
+Given an integer n, generate all structurally unique BST's (binary search trees) that store values 1...n.
+
+For example,
+Given n = 3, your program should return all 5 unique BST's shown below.
+1         3     3      2      1
+ \       /     /      / \      \
+  3     2     1      1   3      2
+ /     /       \                 \
+2     1         2                 3
+*/
+
+class Solution {
+public:
+    struct TreeNode {
+        int val;
+        TreeNode *left;
+        TreeNode *right;
+        TreeNode(int x) : val(x), left(NULL), right(NULL) {}        
+    };
+    vector<TreeNode*> generateTrees(int n) {
+        vector<TreeNode*> result;
+        if (n == 0)
+            return result;
+        vector<vector<TreeNode*>> dp;
+        dp.push_back({ NULL });
+        for (int i = 1; i <= n; ++i) {
+            vector<TreeNode*> temp;
+            for (int j = 1; j <= i; ++j) {
+                for (auto &left : dp[j - 1]) {
+                    for (auto &right : dp[i - j]) {
+                        TreeNode* root = new TreeNode(j);
+                        root->left = CopyTree(left, 0);
+                        root->right = CopyTree(right, j);
+                        temp.push_back(root);
+                    }
+                }
+            }
+            dp.push_back(temp);
+        }
+        for (int i = 1; i < dp.size() - 1; ++i) {
+            for (auto &root : dp[i])
+                FreeTree(root);
+        }
+        return dp.back();
+    }
+    TreeNode* CopyTree(TreeNode* root, int incre) {
+        if (root) {
+            TreeNode* copyroot = new TreeNode(root->val + incre);
+            copyroot->left = CopyTree(root->left, incre);
+            copyroot->right = CopyTree(root->right, incre);
+            return copyroot;
+        }
+        else
+            return NULL;
+    }
+    void FreeTree(TreeNode* root) {
+        if (root->left)
+            FreeTree(root->left);
+        if (root->right)
+            FreeTree(root->right);
+        delete root;
+    }
+    void Print(TreeNode* root) {
+        if (root) {
+            cout << root->val << " ";
+            Print(root->left);           
+            Print(root->right);
+        }
+    }
+};
+int main()
+{  
+    Solution s;
+    auto result = s.generateTrees(3);
+    for (auto &root : result) {
+        s.Print(root);
+        cout << endl;
+        s.FreeTree(root);
+    }
+    system("pause");
+    return 0;
+}
--- 096-Unique Binary Search Trees/096.cpp
+++ 096-Unique Binary Search Trees/096.cpp
@@ -2,16 +2,14 @@
 #include <vector>
 using namespace std;
 /*
-Given a binary tree, return the inorder traversal of its nodes' values.
-For example:
-Given binary tree [1,null,2,3],
-1
- \
-  2
- /
-3
-return [1,3,2].
-Note: Recursive solution is trivial, could you do it iteratively?
+Given n, how many structurally unique BST's (binary search trees) that store values 1...n?
+For example,
+Given n = 3, there are a total of 5 unique BST's. 
+   1         3     3      2      1
+    \       /     /      / \      \
+     3     2     1      1   3      2
+    /     /       \                 \
+   2     1         2                 3
 */
 
 class Solution {
