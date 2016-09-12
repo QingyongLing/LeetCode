6d084f6502c0bdcd5b04e655903d5fb7af2ed842
Ling <2682451149@qq.com>
2016-09-12 10:56:08 +0800

    135

---
+++ 135-Candy/135.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+/*
+There are N children standing in a line. Each child is assigned a rating value.
+You are giving candies to these children subjected to the following requirements:
+•Each child must have at least one candy.
+•Children with a higher rating get more candies than their neighbors.
+What is the minimum candies you must give?
+*/
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        if (ratings.empty())
+            return 0;
+        vector<int> leftcount(ratings.size(), 0);
+        vector<int> rightcount(ratings.size(), 0);
+        int temp = 0;
+        for (int i = 1; i < ratings.size(); ++i) {
+            if (ratings[i] > ratings[i - 1])
+                ++temp;
+            else
+                temp = 0;          
+            leftcount[i] = temp;
+        }
+        temp = 0;
+        for (int i = ratings.size() - 2; i >= 0; --i) {
+            if (ratings[i] > ratings[i + 1])
+                ++temp;
+            else
+                temp = 0;          
+            rightcount[i] = temp;
+        }
+        int result = 0;
+        for (int i = 0; i < leftcount.size(); ++i) {
+            result += max(leftcount[i], rightcount[i]);
+        }
+        return result + leftcount.size();
+    }
+};
+int main() {
+    Solution s;
+    vector<int> ratings = { 1, 2, 2 };
+    cout << s.candy(ratings) << endl;   
+    system("pause");
+    return 0;
+}
