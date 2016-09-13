70304f09d3f564cfea1414df7428ea5b32239d57
Ling <1027837999@qq.com>
2016-09-13 11:42:33 +0800

    128

---
+++ 128-Longest Consecutive Sequence/128.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_set>
+using namespace std;
+/*
+Given an unsorted array of integers, find the length of the longest consecutive elements sequence.
+For example,
+Given [100, 4, 200, 1, 3, 2],
+The longest consecutive elements sequence is [1, 2, 3, 4]. Return its length: 4.
+Your algorithm should run in O(n) complexity.
+*/
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        if (nums.empty())
+            return 0;
+        unordered_set<int> table;
+        for (auto &n : nums)
+            table.insert(n);
+        int result = 0;
+        for (auto &n : nums) {
+            int curright = n, len = 0;
+            while (1) {
+                auto iter = table.find(++curright);
+                if (iter != table.end())
+                    table.erase(iter);
+                else break;
+            }
+            int curleft = n;
+            while (1) {
+                auto iter = table.find(--curleft);
+                if (iter != table.end())
+                    table.erase(iter);
+                else break;
+            }
+            result = max(result, curright - curleft - 1);
+        }
+        return result;
+    }
+};
+int main() {
+    Solution s;
+    vector<int> gas = { 0,3,7,2,5,8,4,6,0,1 };
+    cout << s.longestConsecutive(gas) << endl;
+    system("pause");
+    return 0;
+}
