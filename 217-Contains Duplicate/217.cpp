4d1c62b3e66cb9627e323794573b2d838158b896
Ling <1027837999@qq.com>
2016-09-26 16:05:46 +0800

    217

---
+++ 217-Contains Duplicate/217.cpp
@@ -0,0 +1,28 @@
+﻿#include <vector>
+#include <unordered_set>
+#include <iostream>
+using namespace std;
+/*
+Given an array of integers, find if the array contains any duplicates. 
+Your function should return true if any value appears at least twice in the array,
+and it should return false if every element is distinct.
+*/
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> hashset;
+        for (auto &n : nums) {
+            if (hashset.find(n) != hashset.end())
+                return true;
+            else
+                hashset.insert(n);
+        }
+        return false;
+    }
+};
+int main() {
+    Solution s; 
+    
+    system("pause");
+    return 0;
+}
