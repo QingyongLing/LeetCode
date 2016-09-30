5f843bc8222245a73f2348fab40c58b144668c50
Ling <1027837999@qq.com>
2016-09-30 22:52:17 +0800

    226

---
+++ 226-Invert Binary Tree/226.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+using namespace std;
+/*
+Invert a binary tree.
+        4
+      /   \
+     2     7
+    / \   / \
+   1   3 6   9
+to      4
+      /   \
+     7     2
+    / \   / \
+   9   6 3   1
+Trivia:
+This problem was inspired by this original tweet by Max Howell:
+Google: 90% of our engineers use the software you wrote (Homebrew), but you can’t invert a binary tree on a whiteboard so fuck off.
+*/
+class Solution {
+public:
+    struct TreeNode {
+        int val;
+        TreeNode *left;
+        TreeNode *right;
+        TreeNode(int x) : val(x), left(NULL), right(NULL) {}       
+    };
+    TreeNode* invertTree(TreeNode* root) {
+        if (root == NULL) return NULL;
+        swap(root->left, root->right);
+        root->left = invertTree(root->left);
+        root->right = invertTree(root->right);
+        return root;
+    }
+};
+int main() {
+    
+    system("pause");
+    return 0;
+}
