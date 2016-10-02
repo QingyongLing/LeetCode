10a1813809ab02d6bec75998a61fb45fc288ce04
Ling <1027837999@qq.com>
2016-10-02 22:36:19 +0800

    237

---
+++ 237-Delete Node in a Linked List/237.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+/*
+Write a function to delete a node (except the tail) in a singly linked list, given only access to that node.
+Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third node with value 3, the linked list should become 1 -> 2 -> 4 after calling your function.
+*/
+class Solution {
+public:
+    struct ListNode {
+        int val;
+        ListNode *next;
+        ListNode(int x) : val(x), next(NULL) {}
+    };
+    void deleteNode(ListNode* node) {
+        ListNode* next = node;
+        do{
+            node = next;
+            next = next->next;
+            node->val = next->val; 
+        } while (next->next != NULL);
+        node->next = NULL;
+        delete next;
+    }
+};
+int main() {    
+    system("pause");
+    return 0;
+}
