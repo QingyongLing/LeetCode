a6fa53a96a16351b4af7b5e996955c17b9c933fd
Ling <2682451149@qq.com>
2016-10-04 17:17:07 +0800

    fix

--- 074-Search a 2D Matrix/074.cpp
+++ 074-Search a 2D Matrix/074.cpp
@@ -19,74 +19,23 @@ Given target = 3, return true.
 
 class Solution {
 public:
-    int x1, x2, y1, y2;
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        if (matrix.size() == 0)
-            return false;
-        x1 = y1 = 0;
-        x2 = matrix.size() - 1;
-        y2 = matrix[0].size() - 1;
-        while (x1 <= x2&&y1 <= y2)
-        {
-            x2 = colbinarysearch(matrix, target, y1);
-            y1 = rowbinarysearch(matrix, target, x2);
-            x1 = colbinarysearch(matrix, target, y2);
-            y2 = rowbinarysearch(matrix, target, x1);
-            if (x1 == x2&& y1 == y2)
-            {
-                if (target == matrix[x1][y1])
-                    return true;
-                break;
-            }
-            if (x1 <= x2 - 1 && y1 <= y2 - 1)
-                break;
+    int row = matrix.size(), col = matrix[0].size();
+        int lo = 0, hi = row*col - 1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            int num = getelem(mid, matrix);
+            if (num > target)
+                hi = mid - 1;
+            else if (num < target)
+                lo = mid + 1;
+            else return true;
         }
-        if (matrix[x1][y1] == target || matrix[x2][y1] == target || matrix[x1][y2] == target || matrix[x2][y2] == target)
-            return true;
-        else
-            return false;
+        return false;
     }
-    //find the first value bigger than target
-    //use upper_bound may be better
-    int rowbinarysearch(vector<vector<int>>& matrix, int target,int row)
-    {
-        int left = y1, right = y2, mid;
-        if (matrix[row][right] <= target)
-            return right;
-        if (matrix[row][left] >= target)
-            return left;
-        while (left < right)
-        {
-            mid = (left + right) / 2;
-            if (mid != 0 && matrix[row][mid] >= target&&matrix[row][mid - 1] < target)
-                return mid;
-            if (matrix[row][mid] > target)
-                right = mid - 1;
-            else
-                left = mid + 1;
-        }
-        return right;
-    }
-    //find the first value bigger than target
-    //use upper_bound may be better
-    int colbinarysearch(vector<vector<int>>& matrix, int target, int col)
-    {
-        int top = x1, bottom = x2, mid;
-        if (matrix[bottom][col] <= target)
-            return bottom;
-        if (matrix[top][col] >= target)
-            return top;
-        while (top < bottom)
-        {
-            mid = (top + bottom) / 2;
-            if (mid != 0 && matrix[mid][col] >= target&&matrix[mid - 1][col] < target)
-                return mid;
-            if (matrix[mid][col] > target)
-                bottom = mid - 1;
-            else
-                top = mid + 1;
-        }
-        return bottom;
+private:
+    int getelem(int index, vector<vector<int>>& matrix) {
+        int col = matrix[0].size();
+        return matrix[index / col][index%col];
     }
 };
 int main()
