a06c6bf8bc5168ee6a747384a31f06aa5f838fed
Ling <2682451149@qq.com>
2016-10-04 22:33:52 +0800

    260

--- 137-Single Number II/137.cpp
+++ 137-Single Number II/137.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 using namespace std;
 /*
-Given an array of integers, every element appears twice except for one. Find that single one.
+Given an array of integers, every element appears three times except for one. Find that single one.
 Note:
 Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
 */
