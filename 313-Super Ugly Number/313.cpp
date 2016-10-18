039c71ddab818c8fa3f6775a92c13271311a6916
Ling <2682451149@qq.com>
2016-10-18 16:25:44 +0800

    313

--- 313-Super Ugly Number/313.cpp
+++ 313-Super Ugly Number/313.cpp
@@ -15,11 +15,39 @@ Special thanks to @dietpepsi for adding this problem and creating all test cases
 
 class Solution {
 public:
+    struct Elem
+    {
+        int nextUglynum, index, prime;
+        Elem():nextUglynum(0), index(0), prime(0){}
+    };
+    //slower orz
     int nthSuperUglyNumber(int n, vector<int>& primes) {
-        vector<int> index(primes.size(), 0), result(n);
+        if (n == 1) return 1;
+        vector<Elem> uglynums(primes.size());
+        for (int i = 0; i < primes.size(); ++i) {
+            uglynums[i].nextUglynum = primes[i];
+            uglynums[i].prime = primes[i];
+        }
+        int cur = 0;
+        vector<int> result(n);
+        auto cmp = [](Elem &a, Elem &b) { return a.nextUglynum > b.nextUglynum; };
+        make_heap(uglynums.begin(), uglynums.end(), cmp);
+        for (int i = 1; i < n; ++i) {
+            int temp = uglynums.front().nextUglynum;
+            if (cur == temp) --i;
+            else result[i] = temp;
+            cur = temp;
+            pop_heap(uglynums.begin(), uglynums.end(), cmp);
+            Elem &last = uglynums.back();
+            last.nextUglynum = result[++last.index] * last.prime;
+            push_heap(uglynums.begin(), uglynums.end(), cmp);
+        }
+        return result.back();
+    }
+    int nthSuperUglyNumber2(int n, vector<int>& primes) {
+        vector<int> index(primes.size(), 0), result(n, INT_MAX);
         result[0] = 1;
         for (int i = 1; i < n; ++i) {
-            result[i] = INT_MAX;
             for (int j = 0; j < index.size(); ++j) 
                 result[i] = min(result[i], primes[j] * result[index[j]]);            
             for (int j = 0; j < index.size(); ++j)
