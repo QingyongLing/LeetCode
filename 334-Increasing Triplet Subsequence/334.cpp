b0f35900eb80f9a6de12f9a46796abf9fca883e2
Ling <2682451149@qq.com>
2016-11-17 19:14:16 +0800

    334

---
+++ 334-Increasing Triplet Subsequence/334.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+/*
+Given an unsorted array return whether an increasing subsequence of length 3 exists or not in the array.
+Formally the function should:
+Return true if there exists i, j, k
+such that arr[i] < arr[j] < arr[k] given 0 ≤ i < j < k ≤ n-1 else return false.
+Your algorithm should run in O(n) time complexity and O(1) space complexity.
+Examples:
+Given [1, 2, 3, 4, 5],
+return true.
+Given [5, 4, 3, 2, 1],
+return false.
+Credits:
+Special thanks to @DjangoUnchained for adding this problem and creating all test cases.
+*/
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        vector<int> lis;
+        for (int i : nums) {
+            if (lis.empty() || i > lis.back())
+                lis.push_back(i);
+            else {
+                for (int &n : lis) {
+                    if (n >= i) {
+                        n = i;
+                        break;
+                    }
+                }
+            }
+            if (lis.size() == 3)
+                return true;
+        }
+        return false;
+    }
+};
+int main() {
+    Solution s;
+    vector<int> vec = { 1,2,3,4,5 };
+    cout << s.increasingTriplet(vec) << endl;
+    return 0;
+}
