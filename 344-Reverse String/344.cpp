d608a1d9002c9cb96c6ad0bc80a90aa1f382dae2
Ling <2682451149@qq.com>
2016-11-20 17:46:45 +0800

    344

---
+++ 344-Reverse String/344.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+/*
+Write a function that takes a string as input and returns the string reversed.
+Example:
+Given s = "hello", return "olleh".
+*/
+class Solution {
+public:
+    string reverseString(string s) {
+        int lo = 0, hi = s.size() - 1;
+        while (lo<hi) {
+            swap(s[lo++], s[hi--]);
+        }
+        return s;
+    }
+};
+int main() {
+    Solution s;
+    return 0;
+}
