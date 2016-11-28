1782363ab6d1e2b40010cb228258da270d706762
Ling <2682451149@qq.com>
2016-11-28 20:53:33 +0800

    374

---
+++ 374-Guess Number Higher or Lower/374.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std;
+/*
+We are playing the Guess Game. The game is as follows:
+I pick a number from 1 to n. You have to guess which number I picked.
+Every time you guess wrong, I'll tell you whether the number is higher or lower.
+You call a pre-defined API guess(int num) which returns 3 possible results (-1, 1, or 0):
+-1 : My number is lower
+1 : My number is higher
+0 : Congrats! You got it!
+Example:
+n = 10, I pick 6.
+Return 6.
+*/
+
+// Forward declaration of guess API.
+// @param num, your guess
+// @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+int guess(int num);
+class Solution {
+public:
+    int guessNumber(int n) {
+        int lo = 1, hi = n;
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            int temp = guess(mid);
+            if (temp == 0) return mid;
+            else if (temp == 1) lo = mid + 1;
+            else hi = mid - 1;
+        }
+        return lo;
+    }
+};
+
+int main() {
+    Solution s;
+    return 0;
+}
