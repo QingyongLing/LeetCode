bf4a16f6048352600b3755f631dfedec823c60f1
Ling <2682451149@qq.com>
2016-11-30 21:26:15 +0800

    384

---
+++ 384-Shuffle an Array/384.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <vector>
+#include <time.h>
+using namespace std;
+/*
+Shuffle a set of numbers without duplicates.
+Example:
+// Init an array with set 1, 2, and 3.
+int[] nums = {1,2,3};
+Solution solution = new Solution(nums);
+// Shuffle the array [1,2,3] and return its result. Any permutation of [1,2,3] must equally likely to be returned.
+solution.shuffle();
+// Resets the array back to its original configuration [1,2,3].
+solution.reset();
+// Returns the random shuffling of array [1,2,3].
+solution.shuffle();
+*/
+
+class Solution {
+public:
+    Solution(vector<int> nums) {
+        num = nums;
+        suff = nums;
+        srand(time(NULL));
+    }
+
+    /** Resets the array to its original configuration and return it. */
+    vector<int> reset() {
+        return num;
+    }
+
+    /** Returns a random shuffling of the array. */
+    vector<int> shuffle() {
+
+        if (!suff.empty()) {
+            int n1 = rand() % suff.size();
+            int n2 = rand() % suff.size();
+            swap(suff[n1], suff[n2]);
+        }
+        return suff;
+    }
+private:
+    vector<int> num;
+    vector<int> suff;
+};
+
+
+int main() {
+    return 0;
+}
---
+++ 385-Mini Parser/385.cpp
@@ -0,0 +1,89 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <time.h>
+using namespace std;
+/*
+Given a nested list of integers represented as a string, implement a parser to deserialize it.
+Each element is either an integer, or a list -- whose elements may also be integers or other lists.
+Note: You may assume that the string is well-formed:
+•String is non-empty.
+•String does not contain white spaces.
+•String contains only digits 0-9, [, - ,, ].
+Example 1:
+Given s = "324",
+You should return a NestedInteger object which contains a single integer 324.
+Example 2:
+Given s = "[123,[456,[789]]]",
+Return a NestedInteger object containing a nested list with 2 elements:
+1. An integer containing value 123.
+2. A nested list containing two elements:
+i.  An integer containing value 456.
+ii. A nested list with one element:
+a. An integer containing value 789.
+*/
+
+// This is the interface that allows for creating nested lists.
+// You should not implement it, or speculate about its implementation
+class NestedInteger {
+public:
+        // Constructor initializes an empty nested list.
+        NestedInteger();
+        // Constructor initializes a single integer.
+        NestedInteger(int value);
+        // Return true if this NestedInteger holds a single integer, rather than a nested list.
+        bool isInteger() const;
+        // Return the single integer that this NestedInteger holds, if it holds a single integer
+        // The result is undefined if this NestedInteger holds a nested list
+        int getInteger() const;
+        // Set this NestedInteger to hold a single integer.
+        void setInteger(int value);
+        // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+        void add(const NestedInteger &ni);
+        // Return the nested list that this NestedInteger holds, if it holds a nested list
+        // The result is undefined if this NestedInteger holds a single integer
+        const vector<NestedInteger> &getList() const;
+};
+
+class Solution {
+public:
+    NestedInteger deserialize(string s) {
+        NestedInteger result;
+        vector<string> strs;
+        int index = 0;
+        if (s[index] != '[')
+            return NestedInteger(stoi(s));
+        else
+            ++index;
+        int num = 0;
+        bool islist = false;
+        for (int i = index; i < s.size(); ++i) {
+            if (islist) {
+                if (s[i] == '[')++num;
+                if (s[i] == ']')--num;
+                if (num == 0) {
+                    result.add(deserialize(s.substr(index, i - index + 1)));
+                    islist = false;
+                    index = i + 2;
+                }
+            }
+            else {
+                if (s[i] == '[') {
+                    ++num;
+                    islist = true;
+                }
+                if (s[i] == ',' || s[i] == ']') {
+                    if (i > index) {
+                        result.add(NestedInteger(stoi(s.substr(index, i - index))));
+                        index = i + 1;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+};
+
+int main() {
+    return 0;
+}
