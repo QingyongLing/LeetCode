077a9b9e3ee9e4e77f2456db8b72e82499bebfb0
Ling <2682451149@qq.com>
2016-11-30 16:37:47 +0800

    383

---
+++ 383-Ransom Note/383.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+/*
+Given an arbitrary ransom note string and another string containing letters from all the 
+magazines, write a function that will return true if the ransom note can be constructed 
+from the magazines ; otherwise, it will return false.
+Each letter in the magazine string can only be used once in your ransom note.
+Note:
+You may assume that both strings contain only lowercase letters.
+canConstruct("a", "b") -> false
+canConstruct("aa", "ab") -> false
+canConstruct("aa", "aab") -> true
+*/
+
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        int rans[26] = {}, maga[26] = {};
+        for (auto c : ransomNote) ++rans[c - 'a'];
+        for (auto c : magazine) ++maga[c - 'a'];
+        for (int i = 0; i < 26; ++i) {
+            if (rans[i] > maga[i])
+                return false;
+        }
+        return true;
+    }
+};
+
+int main() {
+    Solution s;
+    cout << s.canConstruct("aa", "aab") << endl;
+    return 0;
+}
