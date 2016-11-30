8d8bdf762dde314dd6101c6d0418387b10649ae1
Ling <2682451149@qq.com>
2016-11-30 21:37:41 +0800

    387

---
+++ 387-First Unique Character in a String/387.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+/*
+Given a string, find the first non-repeating character in it and return it's index.
+If it doesn't exist, return -1.
+Examples:
+s = "leetcode"
+return 0.
+s = "loveleetcode",
+return 2.
+Note: You may assume the string contain only lowercase letters
+*/
+
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int count[26] = {};
+        for (char c : s) ++count[c - 'a'];
+        for (int i = 0; i < s.size(); ++i)
+            if (count[s[i] - 'a'] == 1) return i;
+        return -1;
+    }
+};
+
+int main() {
+    Solution s;
+    cout << s.firstUniqChar("leetcode") << endl;
+    return 0;
+}
