62314db34ebc3790e1abafac51273485db8cd8b7
Ling <2682451149@qq.com>
2016-12-03 22:05:36 +0800

    401 402

---
+++ 401-Binary Watch/401.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+/*
+A binary watch has 4 LEDs on the top which represent the hours (0-11), and the 6 LEDs on the bottom represent 
+the minutes (0-59).
+Each LED represents a zero or one, with the least significant bit on the right.
+For example, the above binary watch reads "3:25".
+Given a non-negative integer n which represents the number of LEDs that are currently on, return all possible
+times the watch could represent.
+Example:
+Input: n = 1
+Return: ["1:00", "2:00", "4:00", "8:00", "0:01", "0:02", "0:04", "0:08", "0:16", "0:32"]
+Note:
+•The order of output does not matter.
+•The hour must not contain a leading zero, for example "01:00" is not valid, it should be "1:00".
+•The minute must be consist of two digits and may contain a leading zero, for example "10:2" is not valid, 
+it should be "10:02".
+*/
+
+class Solution {
+public:
+    vector<string> readBinaryWatch(int num) {
+        vector<int> time{ 1,2,4,8,1,2,4,8,16,32 }, chosen;
+        vector<string> result;
+        backtracking(result, time, chosen, 0, num);
+        return result;
+    }
+private:
+    void backtracking(vector<string>& result, vector<int> &time, vector<int> &chosen, int index,int num) {
+        if (chosen.size() == num) {
+            int hour = 0, minute = 0;
+            for (int i : chosen) {
+                if (i < 4)hour += time[i];
+                else minute += time[i];
+            }
+            if (hour > 11 || minute > 59) return;
+            string str;
+            if (hour)str.append(to_string(hour));
+            else str.push_back('0');
+            str.push_back(':');
+            if (minute == 0)
+                str.append("00");
+            else {
+                if (minute < 10)
+                    str.push_back('0');
+                str.append(to_string(minute));
+            }
+            result.push_back(str);
+        }
+        else {
+            for (int i = index; i < time.size(); ++i) {
+                chosen.push_back(i);
+                backtracking(result, time, chosen, i + 1, num);
+                chosen.pop_back();
+            }
+        }
+    }
+};
+
+int main() {
+    Solution s;
+    for (auto &s : s.readBinaryWatch(1))
+        cout << s << "  ";
+    return 0;
+}
---
+++ 402-Remove K Digits/402.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+/*
+Given a non-negative integer num represented as a string, remove k digits from the number so that
+the new number is the smallest possible.
+Note:
+•The length of num is less than 10002 and will be ≥ k.
+•The given num does not contain any leading zero.
+Example 1:
+Input: num = "1432219", k = 3
+Output: "1219"
+Explanation: Remove the three digits 4, 3, and 2 to form the new number 1219 which is the smallest.
+Example 2:
+Input: num = "10200", k = 1
+Output: "200"
+Explanation: Remove the leading 1 and the number is 200. Note that the output must not contain leading zeroes.
+Example 3:
+Input: num = "10", k = 2
+Output: "0"
+Explanation: Remove all the digits from the number and it is left with nothing which is 0.
+*/
+
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+        if (k == 0) return num;
+        if (k >= num.size()) return string("0");
+        string result;
+        for (int i = 0; i < num.size(); ++i) {
+            while (k && !result.empty() && num[i] < result.back())
+                --k, result.pop_back();
+            result.push_back(num[i]);
+        }
+        while (k && !result.empty())--k, result.pop_back();
+        auto index = result.find_first_not_of('0');
+        if (index == result.npos)
+            return string("0");
+        else
+            return index == 0 ? result : result.substr(index);
+    }
+};
+
+int main() {
+    Solution s;
+    cout << s.removeKdigits("10200", 2) << endl;
+    return 0;
+}
