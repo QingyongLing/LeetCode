db10c1e75fa264fbf71da15151b4d3ced205d43a
Ling <2682451149@qq.com>
2016-12-11 13:17:41 +0800

    434

---
+++ 434-Number of Segments in a String/434.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+/*
+Count the number of segments in a string, where a segment is defined to be a contiguous
+sequence of non-space characters.
+Please note that the string does not contain any non-printable characters.
+Example:
+Input: "Hello, my name is John"
+Output: 5
+*/
+class Solution {
+public:
+    int countSegments(string s) {
+        int size = s.size(), index = 0, count = 0;
+        while (index < size) {
+            if (s[index] != ' ') {
+                ++count;
+                while (index < size&&s[++index] != ' ');
+            }
+            else ++index;
+        }
+        return count;
+    }
+};
+
+int main() {
+    Solution s;
+    cout << s.countSegments("  Hello , my name is John ! ") << endl;
+}
