e6476254c3489c587cbf2252e43f40629236dd14
Ling <2682451149@qq.com>
2016-12-13 22:40:57 +0800

    437 438

---
+++ 437-Path Sum III/437.cpp
@@ -0,0 +1,78 @@
+﻿#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+/*
+You are given a binary tree in which each node contains an integer value.
+Find the number of paths that sum to a given value.
+The path does not need to start or end at the root or a leaf, but it must go downwards
+(traveling only from parent nodes to child nodes).
+The tree has no more than 1,000 nodes and the values are in the range -1,000,000 to 1,000,000.
+Example:
+root = [10,5,-3,3,2,null,11,3,-2,null,1], sum = 8
+      10
+     /  \
+    5   -3
+   / \    \
+  3   2   11
+ / \   \
+3  -2   1
+Return 3. The paths that sum to 8 are:
+1.  5 -> 3
+2.  5 -> 2 -> 1
+3. -3 -> 11
+*/
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {} 
+};
+class Solution {
+public:
+    int pathSum(TreeNode* root, int sum) {
+        if (root == NULL) return 0;
+        unordered_map<int, int> map;
+        map[0] = 1;
+        return pathsumimpl(map, root, sum, 0);
+    }
+
+    int pathSum2(TreeNode* root, int sum) {
+        if (root == NULL) return 0;
+        vector<int> vec;
+        int count = 0;
+        pathsumimpl2(vec, root, sum, count);
+        return count;
+    }
+private:
+    int pathsumimpl(unordered_map<int, int>& map, TreeNode* root, int sum, int currentsum) {
+        currentsum += root->val;
+        int count = map[currentsum - sum];
+        ++map[currentsum];
+        if (root->left)
+            count += pathsumimpl(map, root->left, sum, currentsum);
+        if (root->right)
+            count += pathsumimpl(map, root->right, sum, currentsum);
+        --map[currentsum];
+        return count;
+    }
+
+    void pathsumimpl2(vector<int> &vec, TreeNode* root, int sum,int &count) {
+        vec.push_back(0);
+        for (auto &i : vec) {
+            i += root->val;
+            if (i == sum)++count;
+        }
+        if (root->left != NULL)
+            pathsumimpl2(vec, root->left, sum, count);
+        if(root->right!=NULL)
+            pathsumimpl2(vec, root->right, sum, count);
+        for (auto &i : vec)
+            i -= root->val;
+        vec.pop_back();
+    }
+};
+int main() {
+    cout <<  endl;
+}
---
+++ 438-Find All Anagrams in a String/438.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+/*
+Given a string s and a non-empty string p, find all the start indices of p's anagrams in s.
+Strings consists of lowercase English letters only and the length of both strings s and p will 
+not be larger than 20,100.
+The order of output does not matter.
+Example 1:
+Input:
+s: "cbaebabacd" p: "abc"
+Output:
+[0, 6]
+Explanation:
+The substring with start index = 0 is "cba", which is an anagram of "abc".
+The substring with start index = 6 is "bac", which is an anagram of "abc".
+Example 2:
+Input:
+s: "abab" p: "ab"
+Output:
+[0, 1, 2]
+Explanation:
+The substring with start index = 0 is "ab", which is an anagram of "ab".
+The substring with start index = 1 is "ba", which is an anagram of "ab".
+The substring with start index = 2 is "ab", which is an anagram of "ab".
+*/
+
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        vector<int> result;
+        if (p.size() > s.size()) return result;
+        vector<int> pcount(26, 0), scount(26, 0);
+        for (int i = 0; i < p.size(); ++i)
+            ++pcount[p[i] - 'a'], ++scount[s[i] - 'a'];
+        int size = s.size() - p.size(), num = p.size();
+        for (int i = 0; i <= size; ++i) {
+            if (pcount == scount) result.push_back(i);
+            --scount[s[i] - 'a'];
+            if (i != size) ++scount[s[i + num] - 'a'];
+        }
+        return result;
+    }
+};
+int main() {
+    cout <<  endl;
+}
