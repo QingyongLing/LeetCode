60665da39cc955bae13bb710a5b6354fcfd07ab6
Ling <2682451149@qq.com>
2017-01-05 17:32:59 +0800

    450

--- 449-Serialize and Deserialize BST/449.cpp
+++ 449-Serialize and Deserialize BST/449.cpp
@@ -4,16 +4,13 @@
 #include <algorithm>
 using namespace std;
 /*
-Given an array of integers where 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice
-and others appear once.
-Find all the elements of [1, n] inclusive that do not appear in this array.
-Could you do it without extra space and in O(n) runtime? You may assume the returned list does 
-not count as extra space.
-Example:
-Input:
-[4,3,2,7,8,2,3,1]
-Output:
-[5,6]
+Serialization is the process of converting a data structure or object into a sequence of bits so that it can be stored in a file or memory buffer,
+ or transmitted across a network connection link to be reconstructed later in the same or another computer environment. 
+Design an algorithm to serialize and deserialize a binary search tree. There is no restriction on how your serialization/deserialization algorithm
+ should work. You just need to ensure that a binary search tree can be serialized to a string and this string can be deserialized to the original 
+tree structure.
+The encoded string should be as compact as possible. 
+Note: Do not use class member/global/static variables to store states. Your serialize and deserialize algorithms should be stateless. 
 */
 
 struct TreeNode {
---
+++ 450-Delete Node in a BST/450.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+using namespace std;
+/*
+Given a root node reference of a BST and a key, delete the node with the given key in the BST.
+Return the root node reference (possibly updated) of the BST.
+Basically, the deletion can be divided into two stages:
+1.Search for a node to remove.
+2.If the node is found, delete the node.
+Note: Time complexity should be O(height of tree).
+Example:
+root = [5,3,6,2,4,null,7]
+key = 3
+    5
+   / \
+  3   6
+ / \   \
+2   4   7
+Given key to delete is 3. So we find the node with value 3 and delete it.
+One valid answer is [5,4,6,2,null,null,7], shown in the following BST.
+    5
+   / \
+  4   6
+ /     \
+2       7
+Another valid answer is [5,2,6,null,4,null,7].
+  5
+ / \
+2   6
+ \   \
+  4   7
+*/
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if (root == NULL) return root;
+        if (root->val == key) {
+            if (root->left&&root->right) {
+                root->val = min(root->right);
+                root->right = deleteNode(root->right, root->val);
+                return root;
+            }
+            else {
+                TreeNode* next = root->left ? root->left : root->right;
+                delete root;
+                return next;
+            }
+        }
+        else if (root->val < key)
+            root->right = deleteNode(root->right, key);
+        else
+            root->left = deleteNode(root->left, key);
+        return root;
+    }
+private:
+    int min(TreeNode* root) {
+        if (root->left) return min(root->left);
+        else return root->val;
+    }
+};
+int main() {
+    
+}
