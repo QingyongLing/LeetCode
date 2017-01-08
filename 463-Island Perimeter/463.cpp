06ae5fd64913e711162032b3fba04a2271949d15
Ling <2682451149@qq.com>
2017-01-08 22:52:28 +0800

    463 475

---
+++ 463-Island Perimeter/463.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+/*
+You are given a map in form of a two-dimensional integer grid where 1 represents land and 0 
+represents water. Grid cells are connected horizontally/vertically (not diagonally). The grid 
+is completely surrounded by water, and there is exactly one island (i.e., one or more connected
+land cells). The island doesn't have "lakes" (water inside that isn't connected to the water 
+around the island). One cell is a square with side length 1. The grid is rectangular, width 
+and height don't exceed 100. Determine the perimeter of the island.
+Example:
+[[0,1,0,0],
+[1,1,1,0],
+[0,1,0,0],
+[1,1,0,0]]
+Answer: 16
+Explanation: The perimeter is the 16 yellow stripes in the image below:
+*/
+
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        if (grid.empty()) return 0;
+        int row = grid.size(), col = grid[0].size();
+        int result = 0;
+        for (int i = 0; i < row; ++i) {
+            for (int j = 0; j < col; ++j) {
+                if (i == 0 && grid[i][j])++result;
+                if (j == 0 && grid[i][j])++result;
+                if (grid[i][j] == 0) {
+                    if (i < row - 1 && grid[i + 1][j])++result;
+                    if (j < col - 1 && grid[i][j + 1])++result;
+                }
+                else {
+                    if (i == row - 1 || (i < row - 1 && !grid[i + 1][j]))
+                        ++result;
+                    if (j == col - 1 || (j < col - 1 && !grid[i][j + 1]))
+                        ++result;
+                }
+            }
+        }
+        return result;
+    }
+};
+int main() {
+    Solution s;
+    
+}
---
+++ 475-Heaters/475.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+/*
+Winter is coming! Your first job during the contest is to design a
+standard heater with fixed warm radius to warm all the houses.
+Now, you are given positions of houses and heaters on a horizontal
+line, find out minimum radius of heaters so that all houses could be covered by those heaters.
+So, your input will be the positions of houses and heaters seperately, and your expected output
+will be the minimum radius standard of heaters.
+Note:
+1.Numbers of houses and heaters you are given are non-negative and will not exceed 25000.
+2.Positions of houses and heaters you are given are non-negative and will not exceed 10^9.
+3.As long as a house is in the heaters' warm radius range, it can be warmed.
+4.All the heaters follow your radius standard and the warm radius will the same.
+Example 1:
+Input: [1,2,3],[2]
+Output: 1
+Explanation: The only heater was placed in the position 2, and if we use the radius 1 standard, 
+then all the houses can be warmed.
+Example 2:
+Input: [1,2,3,4],[1,4]
+Output: 1
+Explanation: The two heater was placed in the position 1 and 4. We need to use radius 1 standard,
+then all the houses can be warmed.
+*/
+
+class Solution {
+public:
+    int findRadius(vector<int>& houses, vector<int>& heaters) {
+        sort(heaters.begin(), heaters.end());
+        vector<int> len(houses.size(), INT_MAX);
+        for (int i = 0; i < houses.size();++i) {
+            auto iter = lower_bound(heaters.begin(), heaters.end(), houses[i]);
+            if (iter == heaters.end())
+                len[i] = houses[i] - heaters.back();
+            else if (iter == heaters.begin())
+                len[i] = heaters.front() - houses[i];
+            else
+                len[i] = min(*iter - houses[i], houses[i] - *(iter - 1));
+        }
+        int result = *max_element(len.begin(), len.end());
+        return result;
+    }
+};
+int main() {
+    Solution s;
+    
+}
